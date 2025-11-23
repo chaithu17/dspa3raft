3a75700d1ea8b4870afba661be6b101ee4dacf1d
chaithu17 <99967190+chaithu17@users.noreply.github.com>
2025-11-23 16:43:45 -0600

    removed raft_implementation

--- raft_implementation/generate_proto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Generate Python gRPC code from proto file
-
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. raft.proto
-
-echo "Generated raft_pb2.py and raft_pb2_grpc.py"
